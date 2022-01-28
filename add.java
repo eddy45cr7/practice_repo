f2c0ac70151a80913da98107c427248f7bfe8dc5
Atharv <atharvp45@gmail.com>
2022-01-28 11:14:26 +0530

    add updated

--- add.java
+++ add.java
@@ -3,6 +3,8 @@ import javax.sound.sampled.SourceDataLine;
 class add{
     public static void main(String[] args) {
         int a,b;
+        a=1;
+        b=2;
         System.out.println("Sum : "+(a+b);
     }
 }
